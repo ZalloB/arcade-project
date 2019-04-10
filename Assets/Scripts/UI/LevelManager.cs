0152c0514a69912288ef4e19037f168386918d8c
Beatriz <beatriz.zallo@gmail.com>
2019-04-10 14:26:04 +0200

    Options panel with basic interaction and AudioMixer configuration

---
+++ Assets/Scripts/UI/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace UI
+{
+    public class LevelManager : MonoBehaviour
+    {
+
+        public void LoadLevel(string name)
+        {
+            SceneManager.LoadScene(name);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
