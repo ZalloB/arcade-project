9510559a03f714d52ae5bb0a5ab71e6ca86c9ec2
Beatriz <beatriz.zallo@gmail.com>
2019-04-22 14:40:48 +0200

    added new content and start scene models

--- Assets/Scripts/MultiLanguage/LanguageManager.cs
+++ Assets/Scripts/MultiLanguage/LanguageManager.cs
@@ -6,6 +6,9 @@ namespace MultiLanguage
 {
     public class LanguageManager : MonoBehaviour
     {
+        
+        private static LanguageManager _instance;
+        
         private Dictionary<string, string[]> lines = new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase);
 
         private string resourceLanguageFolder = "Languages";
@@ -15,10 +18,15 @@ namespace MultiLanguage
         private string defaultLanguage = "en";
 
         private string overrideLanguage = "";
-        
-        private void Awake()
-        {
-            //TODO get's available languages labels 
+
+        private void Awake(){
+            DontDestroyOnLoad (this);
+         
+            if (_instance == null) {
+                _instance = this;
+            } else {
+                Destroy(gameObject);
+            }
         }
     }
 }
