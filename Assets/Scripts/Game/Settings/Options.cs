5e09e919dd5810f8e589771a363532a522f0083c
Beatriz <beatriz.zallo@gmail.com>
2019-04-23 14:04:12 +0200

    options scripts

---
+++ Assets/Scripts/Game/Settings/Options.cs
@@ -0,0 +1,19 @@
+﻿namespace Game.Settings
+{
+    public class Options {
+
+        public bool fullScreen;
+        public int textureQuality;
+        public int antialiasing;
+        public int vSync;
+        public int resolutionIndex;
+
+        #region music
+        public float masterVolume;
+        public float musicVolume;
+        public float sfxVolume;
+        public float ambientVolume;
+        public float playerVolume;
+        #endregion
+    }
+}
