10d6b8d937ee94970f6462d5915c59348efdb1c0
Beatriz <beatriz.zallo@gmail.com>
2019-04-27 19:12:14 +0200

    menu items interaction and calendar manager

---
+++ Assets/Scripts/Game/CalendarGameManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Game
+{
+    public class CalendarGameManager : MonoBehaviour
+    {
+        private readonly DateTime _initialDate = new DateTime(1991, 1, 1);
+
+        private DateTime _actualDate;
+        public int timeVelocity = 5;
+        private void Start()
+        {
+            _actualDate = _initialDate;
+            StartCoroutine(nameof(UpdateCalendar));
+        }
+
+
+        private IEnumerator UpdateCalendar()
+        {
+            while (true)
+            {
+                _actualDate = _actualDate.AddDays(1);
+//                SendMessage("");
+                
+                Debug.Log("UpdateCalendar: " + (int) Time.time);
+                Debug.Log(transformDate());
+                yield return new WaitForSeconds(timeVelocity);
+            }
+        }
+
+
+        private string transformDate()
+        {
+            return $"Day {_actualDate.Day:D} - {_actualDate.Month:G} {_actualDate.Year:D}";
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MenuGameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MenuGameManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Image menuItemImage;
+        [SerializeField]
+        private GameObject menuItemsPanel;
+        
+        
+        public void MenuItemsVisibility()
+        {
+            Debug.Log("Patata.......");
+            var image = menuItemImage;
+            
+            image.color = !menuItemsPanel.activeSelf ? new Color(253, 230,0) : Color.white;
+            
+            menuItemsPanel.SetActive(!menuItemsPanel.activeSelf);
+            
+        }
+    }
+}
